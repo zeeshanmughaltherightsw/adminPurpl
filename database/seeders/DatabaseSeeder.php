e2ffb1109f1751b9d400f2e4b4b610db1c60470f
usmanrehmani <usmanrehmani445566@gmail.com>
2022-11-02 16:48:58 +0500

    reward migrations commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
             PlanSeeder::class,
             ReferralSeeder::class,
             PlanLevelSeeder::class,
+            RewardSeeder::class,
 
         ]);
 
