753bdb43a770884f655a1e79e45d34ba50483a59
Zeeshan Mughal <zeeshansaaab@gmail.com>
2023-01-20 08:30:02 +0500

    Test command added

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Log::info("Test command working fine");
+        return Command::SUCCESS;
+    }
+}
